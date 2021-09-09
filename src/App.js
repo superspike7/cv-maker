658a5013fdeea22b5a57aaafb660cb1b962ee246
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-09 09:23:44 +0800

    fix Profile Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Profile from "./components/Profile"
 class App extends Component {
   render(){
     return (
-    <div className="bg-red-50">
+    <div>
       <h1 className="py-8 text-center text-4xl font-bold">CV maker</h1>
       <div className="w-[80vw] mx-auto shadow-2xl">
         <Profile/>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,45 +2,54 @@ import React from "react";
 import ProfileView from "./ProfileView";
 import ProfileEdit from "./ProfileEdit";
 
-class Profile extends React.Component{
-  constructor(props){
+class Profile extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
       name: "Spike Vinz Cruz",
       title: "Web Developer",
       address: "Liloy, Zamboanga del Norte, Philippines",
-    }
+      edit: false,
+    };
 
     this.handleInput = this.handleInput.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this)
   }
 
-  handleInput(event){
-    const name = event.target.name
-    const value = event.target.value
+  handleInput(event) {
+    const name = event.target.name;
+    const value = event.target.value;
 
     this.setState({
-      [name]: value
-    })
+      [name]: value,
+    });
   }
 
-  render(){
-    const { name, title, address} = this.state
+  toggleEdit(){
+    this.setState(preveState => ({
+      edit: !preveState.edit
+    }));
+  }
+
+  render() {
+    const { name, title, address } = this.state;
+    const editMode = this.state.edit;
     return (
-    <div className="py-4 bg-blue-800 text-white flex flex-col items-center justify-center relative gap-2">
-        <ProfileView
-         name={name}
-         title={title}
-         address={address}/>
-        <ProfileEdit
-         name={name}
-         title={title} 
-         address={address}
-         handleInput={this.handleInput}/>
-
-    </div>
-
+      <div className="py-4 bg-blue-800 text-white flex flex-col items-center justify-center relative gap-2 relative">
+        <button className="absolute px-1 bg-yellow-400 -top-2 -left-2" onClick={this.toggleEdit}>{this.state.edit ? "Done" : "Edit"}</button>
+        {editMode ? (
+          <ProfileEdit
+            name={name}
+            title={title}
+            address={address}
+            handleInput={this.handleInput}
+          />
+        ) : (
+          <ProfileView name={name} title={title} address={address} />
+        )}
+      </div>
     );
-   }
+  }
 }
 export default Profile;
