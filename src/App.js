af16d3c50af6afedc229303195137bfd7a510711
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-11 07:51:12 +0800

    add experienceview component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,69 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ProfileView from "./components/ProfileView";
-import ProfileForm from "./components/ProfileForm" 
+import ProfileForm from "./components/ProfileForm";
+import ExperienceView from "./components/ExperienceView";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       profile: {
         name: "Spike Vinz Cruz",
         title: "Web Developer",
         address: "Liloy, Zamboanga del Norte, Philippines",
-      }
-    }
+      },
+      experience: [
+        {
+          company: "AirBnB",
+          role: "Backend Engineer",
+          date_start: "08-08-18",
+          date_end: "current",
+          location: "Manila, PH",
+          tasks: "I built the entire thing",
+          techStack: "Ruby on Rails"
+        },
+      ],
+    };
 
-    this.handleProfileChange = this.handleProfileChange.bind(this)
+    this.handleProfileChange = this.handleProfileChange.bind(this);
+    this.handleExperienceChange = this.handleExperience.bind(this);
   }
 
-  handleProfileChange(data){
-    return event => {
-      event.preventDefault()
-      console.log(data)
+  handleProfileChange(data) {
+    return (event) => {
+      event.preventDefault();
+      console.log(data);
       this.setState({
-        profile: data
-      })
-    }
+        profile: data,
+      });
+    };
   }
 
+  handleExperience(data) {
+    return (event) => {
+      event.preventDefault();
+      console.log(data);
+      this.setState({
+        experience: data,
+      });
+    };
+  }
 
-  render(){
+  render() {
     return (
-    <div>
-      <h1 className="py-8 text-center text-4xl font-bold">CV maker</h1>
-      <div className="w-[80vw] mx-auto shadow-2xl">
-        <ProfileForm handleSubmit={this.handleProfileChange}/>
-        <br></br>
-        <ProfileView profile={this.state.profile}/>
+      <div>
+        <h1 className="py-8 text-center text-4xl font-bold">CV maker</h1>
+        <div className="w-[80vw] mx-auto shadow-2xl">
+          <ProfileForm handleSubmit={this.handleProfileChange} />
+          {/* <ExperienceForm handleSubmit={this.handleProfileChange}/> */}
+          <br></br>
+          <ProfileView profile={this.state.profile} />
+          <ExperienceView experience={this.state.experience} />
+        </div>
       </div>
-    </div>
     );
-  } 
+  }
 }
-
 
 export default App;
---
+++ src/components/ExperienceView.js
@@ -0,0 +1,28 @@
+function ExperienceView(props){
+  const experiences = props.experience.map((experience, index) => {
+    return (
+      <div className="w-full grid grid-cols-[auto,1fr] p-2 gap-8" key={index}>
+        <div className="w-1/8">
+          <p className="font-light text-xl">{experience.date_start} -- {experience.date_end}</p>
+          <p className="font-light text-xl">{experience.location}</p>
+        </div>
+        <div>
+          <h1 className="font-extrabold text-xl">{experience.company}</h1>
+          <h3 className="font-normal text-xl">{experience.role}</h3>
+          <ul className="list-disc">
+            <li><p className="font-light text-xl">{experience.tasks}</p></li>
+            <li><p className="font-light text-xl">stack used: {experience.stack}</p></li>
+          </ul>
+        </div>
+      </div>
+    )
+  })
+  return (
+    <div className="mt-5">
+      <h1 className="text-center text-4xl font-bold bg-gray-400 mb-5">Experience</h1>
+      {experiences}
+    </div>
+  )
+}
+
+export default ExperienceView 
