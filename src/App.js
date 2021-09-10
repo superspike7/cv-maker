41184e0c759d935ab554263d58f95f13510eaf20
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-10 10:28:13 +0800

    profile section done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,40 @@ import React, {Component} from "react";
 import ProfileView from "./components/ProfileView";
 import ProfileForm from "./components/ProfileForm" 
 
-const PROFILE = {
-    name: "Spike Vinz Cruz",
-    title: "Web Developer",
-    address: "Liloy, Zamboanga del Norte, Philippines",
-}
-
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      profile: {
+        name: "Spike Vinz Cruz",
+        title: "Web Developer",
+        address: "Liloy, Zamboanga del Norte, Philippines",
+      }
+    }
+
+    this.handleProfileChange = this.handleProfileChange.bind(this)
+  }
+
+  handleProfileChange(data){
+    return event => {
+      event.preventDefault()
+      console.log(data)
+      this.setState({
+        profile: data
+      })
+    }
+  }
+
+
   render(){
     return (
     <div>
       <h1 className="py-8 text-center text-4xl font-bold">CV maker</h1>
       <div className="w-[80vw] mx-auto shadow-2xl">
-        <ProfileForm/>
+        <ProfileForm handleSubmit={this.handleProfileChange}/>
         <br></br>
-        <ProfileView profile={PROFILE}/>
+        <ProfileView profile={this.state.profile}/>
       </div>
     </div>
     );
--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -1,35 +1,48 @@
-import React from "react";
+import { useState } from "react";
 
-class ProfileForm extends React.Component{
-  render(){
+function ProfileForm(props) {
+
+  const [profile, setProfile] = useState({})
+
+  function handleInput(event){
+    const {name, value} = event.target
+    setProfile(prevState => ({
+      ...prevState,
+      [name]:value
+    }))
+  }
+
   return (
     <form
       className="p-4 w-1/2 mx-auto flex flex-col bg-blue-500 text-white gap-3"
+      onSubmit={props.handleSubmit(profile)}
     >
       <input
         type="text"
         name="name"
-        defaultValue="John Doe"
+        value={profile.name}
+        onChange={handleInput}
         className="text-4xl bg-transparent border text-center border-green-400"
       ></input>
       <input
         type="text"
         name="title"
-        defaultValue="Web Developer"
+        value={profile.title}
+        onChange={handleInput}
         className="text-xl bg-transparent border text-center border-green-400"
       ></input>
       <input
         type="text"
         name="address"
-        defaultValue="XYZ, Country"
+        value={profile.address}
+        onChange={handleInput}
         className="text-xl bg-transparent border text-center border-green-400"
       ></input>
       <button className="px-1 bg-yellow-500 text-black rounded-md">
-        Save 
+        save
       </button>
     </form>
-    );
-  }
+  );
 }
 
-export default ProfileForm;
+export default ProfileForm
